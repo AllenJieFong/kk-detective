aeae8b91a45c04f78cbb02f1e6b042bab530f415
Patrick.Lin <patrick.lin@PatrickLindeMacBook-Pro.local>
2022-06-24 15:02:14 +0800

    change news item

--- lib/modules/news/view/news_page.dart
+++ lib/modules/news/view/news_page.dart
@@ -29,35 +29,112 @@ class NewsPage extends StatelessWidget {
     );
   }
 
+  // Widget getNewsItem(NewsResponse data, BuildContext context) {
+  //   return Card(
+  //     elevation: 1.0,
+  //     margin: const EdgeInsets.symmetric(vertical: 8),
+  //     shape: const RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.all(
+  //         Radius.circular(16),
+  //       ),
+  //     ),
+  //     child: Container(
+  //       width: double.infinity,
+  //       padding: const EdgeInsets.all(16),
+  //       child: InkWell(
+  //         child: Text(
+  //           data.title ?? "",
+  //           style: const TextStyle(fontSize: 20),
+  //         ),
+  //         onTap: () async {
+  //           if (!await launchUrl(
+  //             // Uri.parse(data.url ?? ""),
+  //             // TODO for demo
+  //             Uri.parse("http://www.google.com"),
+  //             mode: LaunchMode.platformDefault,
+  //           )) {
+  //             throw 'Could not launch ${data.url}';
+  //           }
+  //         },
+  //       ),
+  //     ),
+  //   );
+  // }
+
   Widget getNewsItem(NewsResponse data, BuildContext context) {
-    return Card(
-      elevation: 1.0,
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(16),
-        ),
-      ),
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(16),
-        child: InkWell(
-          child: Text(
-            data.title ?? "",
-            style: const TextStyle(fontSize: 20),
-          ),
-          onTap: () async {
-            if (!await launchUrl(
-              // Uri.parse(data.url ?? ""),
-              // TODO for demo
-              Uri.parse("http://www.google.com"),
-              mode: LaunchMode.platformDefault,
-            )) {
-              throw 'Could not launch ${data.url}';
-            }
-          },
-        ),
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Card(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+        child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [Icon(Icons.tag_faces, size: 80.0)],
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Icon(Icons.label, size: 60.0),
+                    const SizedBox(height: 4),
+                    Text(getDate(data.updateTime ?? 0),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 4),
+                    Text(data.platform ?? "",
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 4),
+                    Text(data.title ?? "",
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 4),
+                    Text(data.content ?? "",
+                        style: const TextStyle(fontSize: 20)),
+                    const SizedBox(height: 8),
+                    Text("更新時間：" + getDate(data.updateTime ?? 0),
+                        style: const TextStyle(fontSize: 20)),
+                    const SizedBox(height: 8),
+                    InkWell(
+                      child: Text(
+                        "連結 ${data.url}",
+                        style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 20,
+                            decoration: TextDecoration.underline),
+                      ),
+                      onTap: () async {
+                        if (!await launchUrl(
+                          Uri.parse(data.url ?? ""),
+                          mode: LaunchMode.platformDefault,
+                        )) {
+                          throw 'Could not launch ${data.url}';
+                        }
+                      },
+                    )
+                  ],
+                ),
+              ],
+            )),
       ),
     );
+  }
+
+  String getDate(int time) {
+    var year = DateTime.fromMillisecondsSinceEpoch(time * 1000).year;
+    var month = DateTime.fromMillisecondsSinceEpoch(time * 1000).month;
+    var day = DateTime.fromMillisecondsSinceEpoch(time * 1000).day;
+    var hour = DateTime.fromMillisecondsSinceEpoch(time * 1000).hour;
+    var minute = DateTime.fromMillisecondsSinceEpoch(time * 1000).minute;
+    return "$year/$month/$day - $hour:$minute";
   }
 }
